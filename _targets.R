c842a509796e0f2cb099ed86336ef972da938c83
Christelle Leitzinger <christelleleitzinger@gmail.com>
2022-03-15 11:42:21 -0400

    Update files in targets

--- _targets.R
+++ _targets.R
@@ -1,5 +1,5 @@
 # Create path
-path <- fs::path("","Volumes","Peres_Research", "K99_R00", "Image analysis data")
+path <- fs::path("","Volumes","Peres_Research", "K99_R00", "Image analysis data", "Spatial Data")
 
 # Load package
 library(targets)
@@ -10,26 +10,83 @@ tar_option_set(packages = c("readr", "tidyverse")) # Package needed to run pipel
 
 # Run pipeline aka used to be the plan.R with drake
 list(
+  # tar_target(
+  #   raw_data_file,
+  #   paste0(path,
+  #   "/op.csv"),
+  #   format = "file"
+  # ),
   tar_target(
-    raw_data_file,
-    paste0(path,
-    "/op.csv"),
-    format = "file"
+    ROI_overall_bivariate,
+    read_csv(paste0(path,
+                    "/Nearest Neighbor/ROI_overall_bivariate.csv"), col_types = cols()) %>%#________________
+      select(suid, slide_type, image.tag, Marker : ncol(.))
   ),
   tar_target(
-    raw_data,
-    read_csv(raw_data_file, col_types = cols())
+    ROI_stroma_bivariate,
+    read_csv(paste0(path,
+                    "/Nearest Neighbor/ROI_stroma_bivariate.csv"), col_types = cols()) %>%
+      select(suid, slide_type, image.tag, anchor : ncol(.))
   ),
   tar_target(
-    data,
-    raw_data %>%
-      select(Cell_Type, stageLocalized)
+    ROI_stroma_univariate,
+    read_csv(paste0(path,
+                    "/Nearest Neighbor/ROI_stroma_univariate.csv"), col_types = cols()) %>%
+      select(suid, slide_type, image.tag, Marker : ncol(.))
+  ),
+  tar_target(
+    ROI_tumor_bivariate,
+    read_csv(paste0(path,
+                    "/Nearest Neighbor/ROI_tumor_bivariate.csv"), col_types = cols()) %>%
+      select(suid, slide_type, image.tag, anchor : ncol(.))
+  ),
+  tar_target(
+    ROI_tumor_univariate,
+    read_csv(paste0(path,
+                    "/Nearest Neighbor/ROI_tumor_univariate.csv"), col_types = cols()) %>%
+      select(suid, slide_type, image.tag, Marker : ncol(.))
+  ),
+  tar_target(
+    K_ROI_overall_bivariate,
+    read_csv(paste0(path,
+                    "/Ripley_K/ROI_overall_bivariate.csv"), col_types = cols()) %>%
+      select(suid, slide_type, image.tag, Marker : ncol(.))
+  ),
+  tar_target(
+    K_ROI_overall_univariate,
+    read_csv(paste0(path,
+                    "/Ripley_K/ROI_overall_univariate.csv"), col_types = cols()) %>%
+      select(suid, slide_type, image.tag, Marker : ncol(.))
+  ),
+  tar_target(
+    K_ROI_stroma_bivariate,
+    read_csv(paste0(path,
+                    "/Ripley_K/ROI_stroma_bivariate.csv"), col_types = cols()) %>%
+      select(suid, slide_type, image.tag, anchor : ncol(.))
+  ),
+  tar_target(
+    K_ROI_stroma_univariate,
+    read_csv(paste0(path,
+                    "/Ripley_K/ROI_stroma_univariate.csv"), col_types = cols()) %>%
+      select(suid, slide_type, image.tag, Marker : ncol(.))
+  ),
+  tar_target(
+    K_ROI_tumor_bivariate,
+    read_csv(paste0(path,
+                    "/Ripley_K/ROI_tumor_bivariate.csv"), col_types = cols()) %>%
+      select(suid, slide_type, image.tag, anchor : ncol(.))
+  ),
+  tar_target(
+    K_ROI_tumor_univariate,
+    read_csv(paste0(path,
+                    "/Ripley_K/ROI_tumor_univariate.csv"), col_types = cols()) %>%
+      select(suid, slide_type, image.tag, Marker : ncol(.))
   )
 )
 
 
 # tar_make() #### NOT RUN in script but must be run in the console
-# tar_load(raw_data) #### NOT RUN in script but must be run in the console
+# tar_load(ROI_overall_bivariate) #### NOT RUN in script but must be run in the console
 
 # drake_clean =
 # tar_destroy()
