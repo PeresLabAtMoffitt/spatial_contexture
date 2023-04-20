4f0b35406f58dd43cc1494341ab8f71384fd642f
chrisleitzinger <christelleleitzinger@gmail.com>
2023-04-20 17:28:01 -0400

    Update targets

--- _targets.R
+++ _targets.R
@@ -1,5 +1,7 @@
 # Create path
-path <- fs::path("","Volumes","Peres_Research", "K99_R00", "Image analysis data", "Spatial Data",
+path1 <- fs::path("","Volumes","Peres_Research", "K99_R00", "Image analysis data", "Spatial Data",
+                 "Alex", "computed", "AACES_ROI1")
+path2 <- fs::path("","Volumes","Peres_Research", "K99_R00", "Image analysis data", "Spatial Data",
                  "Alex", "computed", "AACES_ROI2")
 
 # Import package
@@ -11,97 +13,195 @@ tar_option_set(packages = c("readr", "tidyverse")) # Package needed to run pipel
 
 # Run pipeline aka used to be the plan.R with drake
 list(
+  # ROI1
   tar_target(
-    KNN_ROI_overall_bivariate,
-    read_rds(paste0(path,
+    KNN_ROI1_overall_bivariate,
+    read_rds(paste0(path1,
+                    c("/CD3_fixed_AACES_ROI1_overall_mif.rds")))[["derived"]][["bivariate_NN"]] %>%
+      select("Image Location", "anchor", "counted", "r",
+             "Theoretical CSR", "Permuted G", "Observed G",
+             "Degree of Clustering Permutation", "Degree of Clustering Theoretical")
+  ),
+  tar_target(
+    KNN_ROI1_overall_univariate,
+    read_rds(paste0(path1,
+                    c("/CD3_fixed_AACES_ROI1_overall_mif.rds")))[["derived"]][["univariate_NN"]] %>%
+      select("Image Location", "Marker", "r",
+             "Theoretical CSR", "Permuted CSR", "Observed",
+             "Degree of Clustering Theoretical", "Degree of Clustering Permutation")
+  ),
+  tar_target(
+    C_ROI1_overall_bivariate,
+    read_rds(paste0(path1,
+                    c("/CD3_fixed_AACES_ROI1_overall_mif.rds")))[["derived"]][["bivariate_Count"]] %>%
+      select("Image Location", "anchor", "counted", "r",
+             "Theoretical CSR", "Permuted K", "Observed K",
+             "Degree of Clustering Permutation", "Degree of Clustering Theoretical")
+  ),
+  tar_target(
+    C_ROI1_overall_univariate,
+    read_rds(paste0(path1,
+                    c("/CD3_fixed_AACES_ROI1_overall_mif.rds")))[["derived"]][["univariate_Count"]] %>%
+      select("Image Location", "Marker", "r",
+             "Theoretical CSR", "Permuted K", "Observed K",
+             "Degree of Clustering Permutation", "Degree of Clustering Theoretical")
+  ),
+  tar_target(
+    KNN_ROI1_stroma_bivariate,
+    read_rds(paste0(path1,
+                    c("/CD3_fixed_AACES_ROI1_stroma_mif.rds")))[["derived"]][["bivariate_NN"]] %>%
+      select("Image Location", "anchor", "counted", "r",
+             "Theoretical CSR", "Permuted G", "Observed G",
+             "Degree of Clustering Permutation", "Degree of Clustering Theoretical")
+  ),
+  tar_target(
+    KNN_ROI1_stroma_univariate,
+    read_rds(paste0(path1,
+                    c("/CD3_fixed_AACES_ROI1_stroma_mif.rds")))[["derived"]][["univariate_NN"]] %>%
+      select("Image Location", "Marker", "r",
+             "Theoretical CSR", "Permuted CSR", "Observed",
+             "Degree of Clustering Theoretical", "Degree of Clustering Permutation")
+  ),
+  tar_target(
+    C_ROI1_stroma_bivariate,
+    read_rds(paste0(path1,
+                    c("/CD3_fixed_AACES_ROI1_stroma_mif.rds")))[["derived"]][["bivariate_Count"]] %>%
+      select("Image Location", "anchor", "counted", "r",
+             "Theoretical CSR", "Permuted K", "Observed K",
+             "Degree of Clustering Permutation", "Degree of Clustering Theoretical")
+  ),
+  tar_target(
+    C_ROI1_stroma_univariate,
+    read_rds(paste0(path1,
+                    c("/CD3_fixed_AACES_ROI1_stroma_mif.rds")))[["derived"]][["univariate_Count"]] %>%
+      select("Image Location", "Marker", "r",
+             "Theoretical CSR", "Permuted K", "Observed K",
+             "Degree of Clustering Permutation", "Degree of Clustering Theoretical")
+  ),
+  tar_target(
+    KNN_ROI1_tumor_bivariate,
+    read_rds(paste0(path1,
+                    c("/CD3_fixed_AACES_ROI1_tumor_mif.rds")))[["derived"]][["bivariate_NN"]] %>%
+      select("Image Location", "anchor", "counted", "r",
+             "Theoretical CSR", "Permuted G", "Observed G",
+             "Degree of Clustering Permutation", "Degree of Clustering Theoretical")
+  ),
+  tar_target(
+    KNN_ROI1_tumor_univariate,
+    read_rds(paste0(path1,
+                    c("/CD3_fixed_AACES_ROI1_tumor_mif.rds")))[["derived"]][["univariate_NN"]] %>%
+      select("Image Location", "Marker", "r",
+             "Theoretical CSR", "Permuted CSR", "Observed",
+             "Degree of Clustering Theoretical", "Degree of Clustering Permutation")
+  ),
+  tar_target(
+    C_ROI1_tumor_bivariate,
+    read_rds(paste0(path1,
+                    c("/CD3_fixed_AACES_ROI1_tumor_mif.rds")))[["derived"]][["bivariate_Count"]] %>%
+      select("Image Location", "anchor", "counted", "r",
+             "Theoretical CSR", "Permuted K", "Observed K",
+             "Degree of Clustering Permutation", "Degree of Clustering Theoretical")
+  ),
+  tar_target(
+    C_ROI1_tumor_univariate,
+    read_rds(paste0(path1,
+                    c("/CD3_fixed_AACES_ROI1_tumor_mif.rds")))[["derived"]][["univariate_Count"]] %>%
+      select("Image Location", "Marker", "r",
+             "Theoretical CSR", "Permuted K", "Observed K",
+             "Degree of Clustering Permutation", "Degree of Clustering Theoretical")
+  ),
+  # ROI2
+  tar_target(
+    KNN_ROI2_overall_bivariate,
+    read_rds(paste0(path2,
                     c("/CD3_fixed_AACES_ROI2_overall_mif.rds")))[["derived"]][["bivariate_NN"]] %>%
       select("Image Location", "anchor", "counted", "r",
              "Theoretical CSR", "Permuted G", "Observed G",
              "Degree of Clustering Permutation", "Degree of Clustering Theoretical")
   ),
   tar_target(
-    KNN_ROI_overall_univariate,
-    read_rds(paste0(path,
+    KNN_ROI2_overall_univariate,
+    read_rds(paste0(path2,
                     c("/CD3_fixed_AACES_ROI2_overall_mif.rds")))[["derived"]][["univariate_NN"]] %>%
       select("Image Location", "Marker", "r",
              "Theoretical CSR", "Permuted CSR", "Observed",
              "Degree of Clustering Theoretical", "Degree of Clustering Permutation")
   ),
   tar_target(
-    C_ROI_overall_bivariate,
-    read_rds(paste0(path,
+    C_ROI2_overall_bivariate,
+    read_rds(paste0(path2,
                     c("/CD3_fixed_AACES_ROI2_overall_mif.rds")))[["derived"]][["bivariate_Count"]] %>%
       select("Image Location", "anchor", "counted", "r",
              "Theoretical CSR", "Permuted K", "Observed K",
              "Degree of Clustering Permutation", "Degree of Clustering Theoretical")
   ),
   tar_target(
-    C_ROI_overall_univariate,
-    read_rds(paste0(path,
+    C_ROI2_overall_univariate,
+    read_rds(paste0(path2,
                     c("/CD3_fixed_AACES_ROI2_overall_mif.rds")))[["derived"]][["univariate_Count"]] %>%
       select("Image Location", "Marker", "r",
              "Theoretical CSR", "Permuted K", "Observed K",
              "Degree of Clustering Permutation", "Degree of Clustering Theoretical")
   ),
   tar_target(
-    KNN_ROI_stroma_bivariate,
-    read_rds(paste0(path,
+    KNN_ROI2_stroma_bivariate,
+    read_rds(paste0(path2,
                     c("/CD3_fixed_AACES_ROI2_stroma_mif.rds")))[["derived"]][["bivariate_NN"]] %>%
       select("Image Location", "anchor", "counted", "r",
              "Theoretical CSR", "Permuted G", "Observed G",
              "Degree of Clustering Permutation", "Degree of Clustering Theoretical")
   ),
   tar_target(
-    KNN_ROI_stroma_univariate,
-    read_rds(paste0(path,
+    KNN_ROI2_stroma_univariate,
+    read_rds(paste0(path2,
                     c("/CD3_fixed_AACES_ROI2_stroma_mif.rds")))[["derived"]][["univariate_NN"]] %>%
       select("Image Location", "Marker", "r",
              "Theoretical CSR", "Permuted CSR", "Observed",
              "Degree of Clustering Theoretical", "Degree of Clustering Permutation")
   ),
   tar_target(
-    C_ROI_stroma_bivariate,
-    read_rds(paste0(path,
+    C_ROI2_stroma_bivariate,
+    read_rds(paste0(path2,
                     c("/CD3_fixed_AACES_ROI2_stroma_mif.rds")))[["derived"]][["bivariate_Count"]] %>%
       select("Image Location", "anchor", "counted", "r",
              "Theoretical CSR", "Permuted K", "Observed K",
              "Degree of Clustering Permutation", "Degree of Clustering Theoretical")
   ),
   tar_target(
-    C_ROI_stroma_univariate,
-    read_rds(paste0(path,
+    C_ROI2_stroma_univariate,
+    read_rds(paste0(path2,
                     c("/CD3_fixed_AACES_ROI2_stroma_mif.rds")))[["derived"]][["univariate_Count"]] %>%
       select("Image Location", "Marker", "r",
              "Theoretical CSR", "Permuted K", "Observed K",
              "Degree of Clustering Permutation", "Degree of Clustering Theoretical")
   ),
   tar_target(
-    KNN_ROI_tumor_bivariate,
-    read_rds(paste0(path,
+    KNN_ROI2_tumor_bivariate,
+    read_rds(paste0(path2,
                     c("/CD3_fixed_AACES_ROI2_tumor_mif.rds")))[["derived"]][["bivariate_NN"]] %>%
       select("Image Location", "anchor", "counted", "r",
              "Theoretical CSR", "Permuted G", "Observed G",
              "Degree of Clustering Permutation", "Degree of Clustering Theoretical")
   ),
   tar_target(
-    KNN_ROI_tumor_univariate,
-    read_rds(paste0(path,
+    KNN_ROI2_tumor_univariate,
+    read_rds(paste0(path2,
                     c("/CD3_fixed_AACES_ROI2_tumor_mif.rds")))[["derived"]][["univariate_NN"]] %>%
       select("Image Location", "Marker", "r",
              "Theoretical CSR", "Permuted CSR", "Observed",
              "Degree of Clustering Theoretical", "Degree of Clustering Permutation")
   ),
   tar_target(
-    C_ROI_tumor_bivariate,
-    read_rds(paste0(path,
+    C_ROI2_tumor_bivariate,
+    read_rds(paste0(path2,
                     c("/CD3_fixed_AACES_ROI2_tumor_mif.rds")))[["derived"]][["bivariate_Count"]] %>%
       select("Image Location", "anchor", "counted", "r",
              "Theoretical CSR", "Permuted K", "Observed K",
              "Degree of Clustering Permutation", "Degree of Clustering Theoretical")
   ),
   tar_target(
-    C_ROI_tumor_univariate,
-    read_rds(paste0(path,
+    C_ROI2_tumor_univariate,
+    read_rds(paste0(path2,
                     c("/CD3_fixed_AACES_ROI2_tumor_mif.rds")))[["derived"]][["univariate_Count"]] %>%
       select("Image Location", "Marker", "r",
              "Theoretical CSR", "Permuted K", "Observed K",
