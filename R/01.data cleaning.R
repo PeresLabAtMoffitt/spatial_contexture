bb48164b4ecf23d21840456ecacf4a87c0055638
chrisleitzinger <christelleleitzinger@gmail.com>
2023-04-27 17:11:31 -0400

    Update cleaning fct with fixing ids

--- R/01.data cleaning.R
+++ R/01.data cleaning.R
@@ -17,12 +17,25 @@ clean_data <- function(data) {
     ungroup() %>%
     mutate(across(where(is.numeric), ~ na_if(., NaN))) %>% 
     mutate(image_tag = str_match(image_location, "Analysis Images.(.*?)$")[,2],
+           # keep in case but looks like the mistake was not introduce in the spatial file
            image_tag = str_replace(image_tag, "16-", "16")) %>% 
+    filter(!str_detect(image_tag, "Ctrl")) %>% 
+    # name changed from before
     mutate(suid = str_match(image_tag,
-                            "(Peres_P1_OV|Peres_P1_)([:digit:]*)")[,3],
+                            "(Peres_P1_OV|Peres_P1_AACEES |Peres_P1_AACES |Peres_P1_|Peres_P3_)([:digit:]*)")[,3],
            .before = 1) %>%
-    select(-image_location)
+    # Fix Ids - few Ids are missing a 1 
+    mutate(count_number_in_ids = sapply(str_split(suid, ""), length)) %>% 
+    mutate(suid = case_when(
+      count_number_in_ids == 6         ~ suid,
+      count_number_in_ids == 5 &
+        str_detect(suid, "4....")      ~ suid,
+      count_number_in_ids == 5 &
+        str_detect(suid, "3....")      ~ paste0("1", suid)
+    )) %>% 
+    select(-c(image_location, count_number_in_ids))
 }
+
 
 KNN_ROI1_overall_univariate <- clean_data(KNN_ROI1_overall_univariate)
 write_rds(KNN_ROI1_overall_univariate, "knn_ROI1_overall_univariate.rds")
@@ -31,9 +44,4 @@ C_ROI1_overall_univariate <- clean_data(C_ROI1_overall_univariate)
 write_rds(C_ROI1_overall_univariate, "c_ROI1_overall_univariate.rds")
 
 
-
-
-
-
-
-
+# END Cleaning
